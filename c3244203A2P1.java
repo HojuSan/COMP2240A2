a51502bf189a2ddc559914148a421988a58ba93d
HojuSan <juyong.kim@uon.edu.au>
2018-10-02 20:02:58 +1000

    woo

--- c3244203A2P1.java
+++ c3244203A2P1.java
@@ -1,25 +1,57 @@
 import java.util.concurrent.Semaphore;
 
-class MyClass extends Thread
-{
-    public void run()
-    {
-        for(int i=0; i<10; i++)
-        {
-            System.out.println("Value " + i);
-        }
-    }
-
-}
-
 public class c3244203A2P1
 {
+    int north = 2;
+    int south = 2;
 
     public static void main(String[] args) 
     {
-        MyClass myClass = new MyClass();
-        myClass.run();
+        farmer myClass = new farmer();
+        myClass.start();
+
+        farmer myClass1 = new farmer();
+        myClass1.start();
+
+        farmer myClass2 = new farmer();
+        myClass2.start();
+
+        farmer myClass3 = new farmer();
+        myClass3.start();
 
         System.out.println("working");
     }
+
+    public int getNorth()
+    {
+        return north;
+    }
+
+    public int getSouth()
+    {
+        return south;
+    }
+}
+
+class farmer extends Thread 
+{
+    public void run()
+    {
+        c3244203A2P1 v = new c3244203A2P1();
+
+
+        for(int i=0; i<v.getNorth(); i++)
+        {
+            System.out.println(Thread.currentThread().getId() + "Value " + i);
+        }
+
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+
 }
