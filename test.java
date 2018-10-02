f3e08082503650bbb64008d9df523123dae54dbc
HojuSan <juyong.kim@uon.edu.au>
2018-10-02 23:26:07 +1000

    its getting there

--- test.java
+++ test.java
@@ -7,7 +7,10 @@ public class test
     public static void main(String[] args)
     {
         final Bridge bridge = new Bridge();
+        int nNum =4;
+        int sNum =4;
 
+        //Creates north farmer threads
         Thread thNorthbound = new Thread(new Runnable()
         {
             @Override
@@ -15,42 +18,51 @@ public class test
             {
                 while(true)
                 {
-                    Farmer farmer = new Farmer(bridge);
-                    Thread th = new Thread(farmer);
-                    farmer.setName("North Farmer : " + th.getId());
-                    th.start();
-                    
-                    try 
+                    //generates the farmers based off input
+                    for(int i = 1; i <= nNum;i++)
                     {
-                        TimeUnit.SECONDS.sleep((long)(Math.random()*10));
-                    } 
-                    catch (InterruptedException iex) 
-                    {
-                        //TODO: handle exception
-                        iex.printStackTrace();
+                        Farmer farmer = new Farmer(bridge);
+                        Thread th = new Thread(farmer);
+                        farmer.setName("N_Farmer" + i);
+                        th.start();
+                        
+                        try 
+                        {
+                            TimeUnit.SECONDS.sleep((long)(Math.random()*10));
+                        } 
+                        catch (InterruptedException iex) 
+                        {
+                            //TODO: handle exception
+                            iex.printStackTrace();
+                        }
                     }
                 }
             }
         });
 
-        Thread thSouthbound = new Thread( new Runnable() {
-             
+        //Creates south farmer threads
+        Thread thSouthbound = new Thread( new Runnable() 
+        {
             @Override
             public void run() {
                  
                 while(true)
                 {
-                    Farmer farmer = new Farmer(bridge);
-                    Thread th = new Thread(farmer);
-                    farmer.setName("South Farmer : "+th.getId());
-                    th.start();
-                    try
+                    //generates the farmers based off input
+                    for(int i = 1; i <= sNum;i++)
                     {
-                        TimeUnit.SECONDS.sleep((long)(Math.random()*10));
-                    }
-                    catch(InterruptedException iex)
-                    {
-                        iex.printStackTrace();
+                        Farmer farmer = new Farmer(bridge);
+                        Thread th = new Thread(farmer);
+                        farmer.setName("S_Farmer"+i);
+                        th.start();
+                        try
+                        {
+                            TimeUnit.SECONDS.sleep((long)(Math.random()*10));
+                        }
+                        catch(InterruptedException iex)
+                        {
+                            iex.printStackTrace();
+                        }
                     }
                 }
             }
@@ -59,4 +71,61 @@ public class test
         thNorthbound.start();
         thSouthbound.start();
     }
+}
+
+class Bridge
+{
+    private final Semaphore semaphore;
+
+    public Bridge()
+    {
+        semaphore = new Semaphore(1);
+    }
+
+    public void crossBridge(Farmer farmer)
+    {
+        try 
+        {
+            System.out.printf("%s : Wating for bridge. Going towards ??.\n",farmer.getName());
+            semaphore.acquire();
+            System.out.printf("%s :is crossing the bridge.\n",farmer.getName());
+            long duration = (long)(Math.random() * 10);
+            TimeUnit.SECONDS.sleep(duration);
+        } 
+        catch (InterruptedException iex) 
+        {
+            //TODO: handle exception
+            iex.printStackTrace();
+        }
+        finally
+        {
+            System.out.printf("Farmer %s has crossed the bridge.\n",farmer.getName());
+            semaphore.release();
+        }
+    }
+}
+
+class Farmer implements Runnable
+{
+    private String name;
+    private Bridge bridge;
+     
+    public Farmer(Bridge bridge)
+    {
+        this.bridge = bridge;
+    }
+     
+    public void run()
+    {
+        bridge.crossBridge(this);
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
 }
