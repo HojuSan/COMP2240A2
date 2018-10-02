5d168f3cdbe288d49ef6a091b6fa54c62e79c1ae
HojuSan <juyong.kim@uon.edu.au>
2018-10-02 13:09:30 +1000

    threads

--- c3244203A2P1.java
+++ c3244203A2P1.java
@@ -1,10 +1,25 @@
 import java.util.concurrent.Semaphore;
 
+class MyClass extends Thread
+{
+    public void run()
+    {
+        for(int i=0; i<10; i++)
+        {
+            System.out.println("Value " + i);
+        }
+    }
+
+}
+
 public class c3244203A2P1
 {
 
     public static void main(String[] args) 
     {
-        System.out.println("a1  ");
+        MyClass myClass = new MyClass();
+        myClass.run();
+
+        System.out.println("working");
     }
 }
